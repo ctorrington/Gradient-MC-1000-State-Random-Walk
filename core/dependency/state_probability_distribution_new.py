d9c2fafedccb44e0c6e49b5427b518c30c77f8cb
Christopher Torrington <ctorrington1@gmail.com>
2024-08-10 13:33:25 +0100

    Add Doc strings 🧑‍⚕️

--- core/dependency/state_probability_distribution_new.py
+++ core/dependency/state_probability_distribution_new.py
@@ -4,6 +4,14 @@ from core.dependency.state_index import StateIndex
 from core.dependency.distribution_interface import DistributionInterface
 
 class StateProbabilityDistribution[SI: StateIndex](DistributionInterface[SI, float]):
+    """Probability that a State is achieved following an Action within the 
+    Environment.
+
+    State Probability Distribution is a probability distribution of 
+    successfully achieving a State following an Action from a State within the 
+    Environment.
+    """
+
     def __init__(
         self,
         distribution: Dict[SI, float]
@@ -27,6 +35,15 @@ class StateProbabilityDistribution[SI: StateIndex](DistributionInterface[SI, flo
         key: SI,
         distribution: float
     ) -> None:
+        """Set the probability that a State occurs.
+
+        This probability value is for a State occuring following an Action 
+        from another State within the Environment.
+
+        Args:
+            key (StateIndex): State Index for the State occurring.
+            distribution (float): Probability that a State occurs.
+        """
         if not key in self.keys():
             raise KeyError(f"State Index {key} not within {self.__class__.__name__}.")
 
